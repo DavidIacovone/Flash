f873a8b40d7d957ec7700c1590b86caa8edba599
David Iacovone <iacovone.david@gmail.com>
2022-10-29 20:57:32 +0200

    Adding base component structure and base styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import QuizManager from './QuizManager/QuizManager';
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <QuizManager />
+    </div>
   );
 }
 
---
+++ src/Quiz/Quiz.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Quiz = () => {
+    return (
+        <div>
+            <div className="quiz-top">
+                <select></select>
+                <p>12/20</p>
+            </div>
+            <div className="quiz-middle">
+                <input />
+            </div>
+            <div className="quiz-bottom">
+                <button>Check!</button>
+            </div>
+        </div>
+    )
+}
+
+export default Quiz;
