40c40af9fa94bfa1e07cd0c093d1b1bff81d3984
David Iacovone <iacovone.david@gmail.com>
2022-11-01 22:22:53 +0100

    UI tweaks

--- src/QuizCreator/QuizCreator.js
+++ src/QuizCreator/QuizCreator.js
@@ -1,16 +1,21 @@
 import React from "react";
 import QuizPair from "../QuizPair/QuizPair";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import quizCreator from "./quizCreator.css"
 
 const QuizCreator = () => {
     return (
         <div className="quiz-creator">
+            <Link to={"/"} ><FontAwesomeIcon className="quiz-creator-icon" icon={faArrowLeft} /></Link>
             <div className="quiz-creator-top">
                 <h3 className="quiz-creator-h3">Create a new quiz</h3>
-                <button className="quiz-creator-button">Create</button>
+                <button>Create</button>
             </div>
             <input className="quiz-creator-input" type="text" placeholder="Title.. " />
             <div className="quiz-creator-pairs">
+                <QuizPair />
                 <QuizPair />
             </div>
             <button>+</button>
